f0cf0d8c2b5e27d20c068fea39ca21f068e8c09b
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-01-10 23:33:20 +0100

    Add godotenv and refactor

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,25 +1,13 @@
 package app
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/Adedunmol/mycart/internal/database"
 	"github.com/Adedunmol/mycart/internal/routes"
 	"github.com/Adedunmol/mycart/internal/util"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/httplog/v2"
 )
-
-func Initializers() database.DbInstance {
-	db, err := database.InitDB()
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	return db
-}
 
 func Run(logger *httplog.Logger) {
 
