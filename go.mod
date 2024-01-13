605ecd7f38cd949ff98ace5c5796ab799e8178b7
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-01-13 22:57:50 +0100

    Add registration handler

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.21.4
 require (
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/go-chi/httplog/v2 v2.0.8
-	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.18.2
 	gorm.io/driver/postgres v1.5.4
@@ -32,9 +31,9 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ internal/services/user.go
@@ -0,0 +1,92 @@
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/Adedunmol/mycart/internal/database"
+	"github.com/Adedunmol/mycart/internal/models"
+	"github.com/Adedunmol/mycart/internal/util"
+)
+
+type CreateUserDto struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+}
+
+type APIResponse struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+	Status  string      `json:"status"`
+}
+
+func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var userDto CreateUserDto
+	err := json.NewDecoder(r.Body).Decode(&userDto)
+
+	if _, ok := err.(*json.InvalidUnmarshalError); ok {
+		util.RespondWithJSON(w, http.StatusInternalServerError, "Unable to format the request body")
+		return
+	}
+	if err != nil {
+		util.RespondWithJSON(w, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+
+	var foundUser models.User
+
+	result := database.Database.DB.Where(models.User{Username: userDto.Username}).First(&foundUser)
+
+	if result.Error == nil {
+		util.RespondWithJSON(w, http.StatusConflict, APIResponse{Message: "username already exists", Data: nil, Status: "error"})
+		return
+	}
+
+	roleToAssign := r.URL.String()
+
+	var role models.Role
+
+	if strings.Contains(roleToAssign, "users") {
+		result = database.Database.DB.Where(models.Role{Default: true}).First(&role)
+	} else {
+		result = database.Database.DB.Where(models.Role{Name: "Event-Organizer"}).First(&role)
+	}
+
+	if result.Error != nil {
+		fmt.Println("error looking for the role")
+		util.RespondWithJSON(w, http.StatusInternalServerError, "error looking a role")
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDto.Password), 14)
+
+	if err != nil {
+		fmt.Println("could not hash password", err)
+		util.RespondWithJSON(w, http.StatusInternalServerError, "could not hash password")
+		return
+	}
+
+	user := models.User{
+		FirstName: userDto.FirstName,
+		LastName:  userDto.LastName,
+		Email:     userDto.Email,
+		Password:  string(hashedPassword),
+		Username:  userDto.Username,
+		RoleID:    role.ID,
+	}
+
+	result = database.Database.DB.Create(&user)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		util.RespondWithJSON(w, http.StatusInternalServerError, APIResponse{Message: "error creating user", Data: nil, Status: "error"})
+		return
+	}
+
+	util.RespondWithJSON(w, http.StatusCreated, APIResponse{Message: "", Data: user, Status: "success"})
+}
