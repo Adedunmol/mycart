7b145e7d1a72fdb56e073295d51b9bae6ccca575
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-01-17 22:44:03 +0100

    add login route

--- internal/routes/user.go
+++ internal/routes/user.go
@@ -10,6 +10,7 @@ func UsersRoutes(r *chi.Mux) {
 	userRouter := chi.NewRouter()
 
 	userRouter.Post("/register", services.CreateUserHandler)
+	userRouter.Post("/login", services.LoginUserHandler)
 
 	r.Mount("/users", userRouter)
 }
