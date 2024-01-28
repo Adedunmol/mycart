910612630ac3bf5befe40bba5c5edbb29dc91e3f
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-01-28 23:26:55 +0100

    Add handlers for updating, deleting and getting all products

--- internal/services/product.go
+++ internal/services/product.go
@@ -90,3 +90,91 @@ func GetProductHandler(w http.ResponseWriter, r *http.Request) {
 
 	util.RespondWithJSON(w, http.StatusOK, APIResponse{Message: "", Data: product, Status: "success"})
 }
+
+func GetAllProductsHandler(w http.ResponseWriter, r *http.Request) {
+	var events []models.Product
+
+	database.Database.DB.Where("deleted_at is null").Find(&events)
+
+	util.RespondWithJSON(w, http.StatusOK, APIResponse{Message: "", Data: events, Status: "success"})
+}
+
+func DeleteProductHandler(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+
+	if id == "" {
+		util.RespondWithJSON(w, http.StatusBadRequest, APIResponse{Message: "no product id sent in the url param", Data: nil, Status: "error"})
+		return
+	}
+
+	var event models.Product
+
+	result := database.Database.DB.Where("deleted_at is null").First(&event, id)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		util.RespondWithJSON(w, http.StatusNotFound, APIResponse{Message: "product not found", Data: nil, Status: "error"})
+		return
+	}
+
+	result = database.Database.DB.Delete(&event)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		util.RespondWithJSON(w, http.StatusNotFound, APIResponse{Message: "error deleting product", Data: nil, Status: "error"})
+		return
+	}
+
+	util.RespondWithJSON(w, http.StatusOK, APIResponse{Message: "", Data: event, Status: "success"})
+}
+
+func UpdateEventHandler(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+
+	if id == "" {
+		util.RespondWithJSON(w, http.StatusBadRequest, APIResponse{Message: "no product id sent in the url param", Data: nil, Status: "error"})
+		return
+	}
+
+	var event models.Product
+
+	result := database.Database.DB.Where("deleted_at is null").First(&event, id)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		util.RespondWithJSON(w, http.StatusNotFound, APIResponse{Message: "event not found", Data: nil, Status: "success"})
+		return
+	}
+
+	var eventDto CreateProductDto
+	err := json.NewDecoder(r.Body).Decode(&eventDto)
+
+	if _, ok := err.(*json.InvalidUnmarshalError); ok {
+		fmt.Println(err)
+		util.RespondWithJSON(w, http.StatusInternalServerError, "Unable to format the request body")
+		return
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithJSON(w, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+
+	result = database.Database.DB.Model(&event).Updates(models.Product{
+		Name: eventDto.Name,
+
+		Price:    eventDto.Price,
+		Category: eventDto.Category,
+	})
+
+	if result.Error != nil {
+		fmt.Println(err)
+		util.RespondWithJSON(w, http.StatusInternalServerError, "Error updating event")
+		return
+	}
+
+	util.RespondWithJSON(w, http.StatusOK, APIResponse{Message: "", Data: event, Status: "success"})
+}
