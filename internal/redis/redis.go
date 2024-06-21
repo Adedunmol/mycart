ea2035cc8a8471f2c2996c605229f297a26e6d30
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-06-21 23:24:58 +0100

    Add event listener for expired key events

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -1,8 +1,13 @@
 package redis
 
 import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
 	"sync"
 
+	"github.com/Adedunmol/mycart/internal/logger"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -18,6 +23,39 @@ func Init(redisAddress string) {
 			Password: "",
 			DB:       0,
 		})
+
+		_, err := redisClient.Do(context.Background(), "CONFIG", "SET", "notify-keyspace-events", "KEA").Result()
+
+		if err != nil {
+			logger.Error.Println("error setting up publish event", err)
+		}
+
+		// subscribe to events published in the keyevent channel, specifically for expired events
+		pubsub := redisClient.PSubscribe(context.Background(), "__keyevent@0__:expired")
+
+		for {
+			message, err := pubsub.ReceiveMessage(context.Background())
+
+			if err != nil {
+				logger.Error.Println("error receiving messages from pub/sub channel")
+			}
+
+			fmt.Printf("Keyspace event recieved %v  \n", message.String())
+
+			key := message.Payload
+
+			if strings.Contains(key, "shadowKey") {
+				key = strings.ReplaceAll(key, "shadowKey", "")
+				value := strings.Split(key, ":")
+				userId, _ := strconv.Atoi(value[1])
+
+				_ = GetCart(userId)
+
+				// write cart data to postgres
+
+				DeleteCart(userId)
+			}
+		}
 	})
 }
 
