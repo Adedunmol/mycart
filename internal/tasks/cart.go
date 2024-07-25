8f0fef9f845398e5d0f32904ffbcb7b74e979d78
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-07-25 18:58:42 +0100

    update handler for updating cart

--- internal/tasks/cart.go
+++ internal/tasks/cart.go
@@ -3,12 +3,9 @@ package tasks
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 
-	"github.com/Adedunmol/mycart/internal/database"
-	"github.com/Adedunmol/mycart/internal/models"
 	"github.com/Adedunmol/mycart/internal/redis"
 	"github.com/hibiken/asynq"
 )
@@ -38,17 +35,7 @@ func HandleCartUpdateTask(ctx context.Context, t *asynq.Task) error {
 	}
 	log.Printf("Updating cart for User: user_id=%ds", p.UserID)
 
-	var user models.User
-
-	result := database.DB.First(&user, p.UserID)
-
-	if result.Error != nil {
-		message := fmt.Sprintf("no user found with this id: %d", p.UserID)
-		log.Println(message)
-		return errors.New(message)
-	}
-
-	redis.UpdateCartFromDB(int(user.ID))
+	redis.UpdateCartFromDB(int(p.UserID))
 
 	return nil
 }
