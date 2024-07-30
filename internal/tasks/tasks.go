3a5a20d7feb3376ee3dcf89caf885225d1181f40
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-07-30 14:06:00 +0100

    refactor

--- internal/tasks/tasks.go
+++ internal/tasks/tasks.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"log"
 	"sync"
 
 	"github.com/Adedunmol/mycart/internal/logger"
@@ -11,6 +12,34 @@ var (
 	client *asynq.Client
 	once   sync.Once
 )
+
+const redisAddr = "127.0.0.1:6379"
+
+func Run() {
+	srv := asynq.NewServer(
+		asynq.RedisClientOpt{Addr: redisAddr},
+		asynq.Config{
+			// Specify how many concurrent workers to use
+			Concurrency: 10,
+			// Optionally specify multiple queues with different priority.
+			Queues: map[string]int{
+				"critical": 6,
+				"default":  3,
+				"low":      1,
+			},
+		},
+	)
+
+	// mux maps a type to a handler
+	mux := asynq.NewServeMux()
+	mux.HandleFunc(TypeCartUpdate, HandleCartUpdateTask)
+	mux.HandleFunc(TypeInvoiceGeneration, HandleInvoiceGenerationTask)
+	mux.HandleFunc(TypeEmailDelivery, HandleEmailDeliveryTask)
+
+	if err := srv.Run(mux); err != nil {
+		log.Fatalf("could not run server: %v", err)
+	}
+}
 
 func Init(redisAddress string) {
 	once.Do(func() {
