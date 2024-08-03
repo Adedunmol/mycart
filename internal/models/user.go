fe3ee2cf3de629bf5efaad9c177cd9ccdf13ed09
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-08-03 23:06:19 +0100

    update relationship

--- internal/models/user.go
+++ internal/models/user.go
@@ -6,15 +6,15 @@ import (
 
 type User struct {
 	gorm.Model
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	Username     string `json:"username" gorm:"unique"`
-	Email        string `json:"email" gorm:"unique"`
-	Password     string `json:"-"`
-	Verified     bool   `json:"verified"`
-	RefreshToken string `json:"refresh_token"`
-	// Otp          uint      `json:"otp"`
-	RoleID   uint      `json:"role_id"`
-	Products []Product `gorm:"foreignKey:Vendor"`
-	Orders   []Order   `gorm:"foreignKey:BuyerID"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	Username     string    `json:"username" gorm:"unique"`
+	Email        string    `json:"email" gorm:"unique"`
+	Password     string    `json:"-"`
+	Verified     bool      `json:"verified"`
+	RefreshToken string    `json:"refresh_token"`
+	Otp          Otp       `json:"otp"`
+	RoleID       uint      `json:"role_id"`
+	Products     []Product `gorm:"foreignKey:Vendor"`
+	Orders       []Order   `gorm:"foreignKey:BuyerID"`
 }
